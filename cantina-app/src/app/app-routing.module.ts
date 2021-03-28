95b916f13830b6f270bf90907b85a09a7ec61a65
BiaBibii <beuka_bianka@yahoo.com>
2021-03-28 15:30:45 +0300

    Adding components

--- cantina-app/src/app/app-routing.module.ts
+++ cantina-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContactComponent} from "./contact/contact.component";
+import {DespreComponent} from "./despre/despre.component";
+import {AcasaComponent} from "./acasa/acasa.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo:'acasa', pathMatch:'full'},
+  { path: 'acasa', component: AcasaComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'despre', component: DespreComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cantina-app/src/app/app.module.ts
+++ cantina-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { DespreComponent } from './despre/despre.component';
+import { AcasaComponent } from './acasa/acasa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent,
+    ContactComponent,
+    DespreComponent,
+    AcasaComponent
   ],
   imports: [
     BrowserModule,
