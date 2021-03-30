9148ea69aa3ac1b2c363240b78c9510a5c4f5572
BiaBibii <beuka_bianka@yahoo.com>
2021-03-30 16:56:02 +0300

    update login

--- cantina-app/src/app/app-routing.module.ts
+++ cantina-app/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ContactComponent} from "./contact/contact.component";
-import {DespreComponent} from "./despre/despre.component";
 import {AcasaComponent} from "./acasa/acasa.component";
+import {LoginComponent} from "./login/login.component";
+import {RegisterFormComponent} from "./register-form/register-form.component";
 
 const routes: Routes = [
   { path: '',redirectTo:'acasa', pathMatch:'full'},
   { path: 'acasa', component: AcasaComponent},
   { path: 'contact', component: ContactComponent},
-  { path: 'despre', component: DespreComponent}
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterFormComponent}
 ];
 
 @NgModule({
--- cantina-app/src/app/app.module.ts
+++ cantina-app/src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { ContactComponent } from './contact/contact.component';
-import { DespreComponent } from './despre/despre.component';
 import { AcasaComponent } from './acasa/acasa.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import { AcasaComponent } from './acasa/acasa.component';
     NavBarComponent,
     LoginComponent,
     ContactComponent,
-    DespreComponent,
-    AcasaComponent
+    AcasaComponent,
+    LoginFormComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
