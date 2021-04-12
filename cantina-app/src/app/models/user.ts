4d07edb9cc858060f0f2d0e6f8db4079cf142a5d
Prodan Raul <prodan_raul14@yahoo.com>
2021-04-12 17:48:13 +0300

    Register Service Implemented

---
+++ cantina-app/src/app/models/user.ts
@@ -0,0 +1,17 @@
+export class User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+  username: string;
+
+  constructor(id: number, email: string, first_name: string, last_name: string, password: string, username: string) {
+    this.id = id;
+    this.email = email;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.password = password;
+    this.username = username;
+  }
+}
